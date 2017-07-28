578766b1607cf6640053024b1ce818e4df95a097
Tyler Filla <tylerfilla@gmail.com>
2017-07-28 12:08:07 -0500

    minimum working version

--- main.cpp
+++ main.cpp
@@ -12,29 +12,76 @@
 
 #include <windows.h>
 
+// Uncomment the line below to enable debug prints
+// #define DEBUG
+
+#ifdef DEBUG
+#define Log(x) OutputDebugString(x)
+#else
+#define Log(x)
+#endif
+
 #pragma comment(linker, "/SUBSYSTEM:WINDOWS")
+
+static void ToggleOverview()
+{
+    // Windows 10 has a "Task View" that resembles GNOME 3 Overview
+    // This can be toggled using the Win+Tab key combo
+
+    INPUT event {};
+    event.type = INPUT_KEYBOARD;
+
+    // Press left Windows key
+    event.ki.wVk = VK_LWIN;
+    SendInput(1, &event, sizeof(INPUT));
+
+    // Press tab key
+    event.ki.wVk = VK_TAB;
+    SendInput(1, &event, sizeof(INPUT));
+
+    // Release tab key
+    event.ki.dwFlags = KEYEVENTF_KEYUP;
+    event.ki.wVk = VK_TAB;
+    SendInput(1, &event, sizeof(INPUT));
+
+    // Release left Windows key
+    event.ki.dwFlags = KEYEVENTF_KEYUP;
+    event.ki.wVk = VK_LWIN;
+    SendInput(1, &event, sizeof(INPUT));
+
+    Log(L"Overview toggled\n");
+}
+
+static bool IsOverTriggerArea(LONG x, LONG y)
+{
+    return x < 50 && y < 50;
+}
 
 static void HandleMouseMove(LONG x, LONG y)
 {
     // Whether cursor is currently over trigger area
-    bool is_over = (x < 50) && (y < 50);
+    bool is_over = IsOverTriggerArea(x, y);
 
     // Whether cursor was previously over trigger area
+    // This initializes to the current state to avoid false enter/leave detection on start
     static bool was_over = is_over;
-/*
-    OutputDebugString(L"is: ");
-    OutputDebugString(is_over ? L"yes" : L"no");
-    OutputDebugString(L", was: ");
-    OutputDebugString(was_over ? L"yes" : L"no");
-    OutputDebugString(L"\n");
-*/
+
+    Log(L"Cursor moved (");
+    Log(is_over ? L"is over trigger" : L"not over trigger");
+    Log(is_over == was_over ? L" and " : L" but ");
+    Log(was_over ? L"was over trigger" : L"was not over trigger");
+    Log(L")\n");
+
     if (is_over && !was_over)
     {
-        OutputDebugString(L"mouse entered trigger area\n");
+        Log(L"Cursor entered trigger area\n");
+
+        // Toggle the overview
+        ToggleOverview();
     }
     else if (!is_over && was_over)
     {
-        OutputDebugString(L"mouse left trigger area\n");
+        Log(L"Cursor left trigger area\n");
     }
 
     was_over = is_over;
@@ -42,7 +89,7 @@ static void HandleMouseMove(LONG x, LONG y)
 
 LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    auto event = (MSLLHOOKSTRUCT*) lParam;
+    auto event = reinterpret_cast<MSLLHOOKSTRUCT*>(lParam);
 
     switch (wParam)
     {
@@ -59,13 +106,13 @@ LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    OutputDebugString(L"GNOMEishOverview 0.1.0\n");
+    Log(L"GNOMEishOverview 0.1.0\n");
 
     // Set up low-level mouse hook
     HHOOK hook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, nullptr, 0);
 
     // Operate a message loop on this thread
-    MSG message{};
+    MSG message {};
     while (GetMessage(&message, nullptr, 0, 0))
     {
         DispatchMessage(&message);
